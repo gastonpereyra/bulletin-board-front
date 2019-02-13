8bd017cf067d91345415366870b7d9beeb5881f9
gastonpereyra <rgpxen@gmail.com>
2019-02-13 01:07:33 -0300

    Configurar Apollo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
+// React stuff
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Apollo stuff
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Modulo de App
+import App from './App';
+
+// CSS
+import './index.css';
+
+
+// Configuración de Apollo
+const httpLink = createHttpLink({
+  uri: 'https://bullentin-board-ql.glitch.me/'
+})
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+  })
+
+// Conecto Apollo con los Componentes de React
+const AppConApollo = () => (
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  );
+  
+  // Conectar la App
+  ReactDOM.render(<AppConApollo/>, document.getElementById('root'));
+  serviceWorker.unregister();
